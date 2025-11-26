0e8dc465acff1d45a30a769d9bc5fe6a77595e61
Rishi Soni <sonirishiji@gmail.com>
2025-11-26 14:19:47 +0530

    Add files via upload
    Use these program like a dice of ludo . it is show a random number between 1 to 6 .

---
+++ random_no.py
@@ -0,0 +1,5 @@
+import numpy
+from numpy import random
+# num = random.randint(6)         # for random numbers
+num = random.choice([1,2,3,4,5,6])          # random select one from 1 to 6
+print(num)
