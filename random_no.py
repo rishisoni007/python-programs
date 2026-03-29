05aeba0c3948e0ec4311d862a9657ca304d2006e
Rishi Soni <sonirishiji@gmail.com>
2026-03-29 14:38:20 +0530

    Delete random_no.py

--- random_no.py
+++
@@ -1,5 +0,0 @@
-import numpy
-from numpy import random
-# num = random.randint(6)         # for random numbers
-num = random.choice([1,2,3,4,5,6])          # random select one from 1 to 6
-print(num)
